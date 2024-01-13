8354263480aa136aec5c61e58fa4980c8642ccf0
Tom Angistalis <tom.angistalis@gmail.com>
2024-01-13 17:25:45 +0000

    visionOS support

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,50 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+//https://github.com/realm/realm-swift/releases/download/v10.42.1/RealmSwift@15.0.spm.zip
+
+func buildTargets() -> [Target] {
+    let realmVersion = "v10.45.3"
+    let baseURL = "https://github.com/realm/realm-swift/releases/download/\(realmVersion)"
+
+    let realmChecksum = "db7ed7e71a7d2b17ebb504cf05de1e31b5d7c9a4eee9e908fc88080564f56c25"
+
+    let realmSwift = "https://github.com/realm/realm-swift/releases/download/\(realmVersion)/RealmSwift@15.2.spm.zip"
+    let realmSwiftChecksum = "4baa92fbe999a0240b11bab3246fe16112faa4e42cfe2d9583258ec364ec1340"
+
+    return [
+        .binaryTarget(
+            name: "Realm",
+            url: "\(baseURL)/Realm.spm.zip",
+            checksum: realmChecksum
+        ),
+        .binaryTarget(
+            name: "RealmSwift",
+            url: realmSwift,
+            checksum: realmSwiftChecksum
+        )
+    ]
+}
+
+let package = Package(
+    name: "RealmBinaries",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v4),
+        .visionOS(.v1)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Realm",
+            targets: ["Realm"]),
+        .library(
+            name: "RealmSwift",
+            targets: ["RealmSwift"])
+    ],
+    targets: buildTargets()
+)
